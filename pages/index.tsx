2d70a47d277e25ad7835917104eea06e3bbc8a92
eugenekx <kx.eugene@gmail.com>
2022-08-19 23:16:20 +0300

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+const Home: NextPage = () => {
+	return <div></div>;
+};
+
+export default Home;
